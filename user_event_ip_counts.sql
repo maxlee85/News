7052bd719eb0778b8472443e4312cd738fac15cf
maxlee85 <maxwellclee@gmail.com>
2020-12-27 14:43:09 -0500

    Add files via upload

--- user_event_ip_counts.sql
+++ user_event_ip_counts.sql
@@ -4,7 +4,7 @@ use role etl;
 
 select current_timestamp, 'Creating Staging Table...';
 
-drop table if exists etl.etl_core.user_event_ip_counts
+drop table if exists etl.etl_core.user_event_ip_counts_staging
 
 create table etl.etl_core.user_event_ip_counts_staging
 
@@ -19,15 +19,15 @@ with user_ip_events as (
   left join (select user_id, email from aaptiv_core.aaptiv.users_raw) b
          on a.context_traits_email = b.email
    group by 1,2,3
- union all
-       select coalesce(a.user_id, b.user_id)
-            , a.context_ip as ip
-            , parse_ip(a.context_ip, 'INET')['ipv4']::int as ipv4
-            , count(*) as events
-         from aaptiv_core.segment_ios.tracks a
-    left join (select user_id, email from aaptiv_core.aaptiv.users_raw) b
-           on a.context_traits_email = b.email
-     group by 1,2,3
+  union all
+     select coalesce(a.user_id, b.user_id)
+          , a.context_ip as ip
+          , parse_ip(a.context_ip, 'INET')['ipv4']::int as ipv4
+          , count(*) as events
+       from aaptiv_core.segment_ios.tracks a
+  left join (select user_id, email from aaptiv_core.aaptiv.users_raw) b
+         on a.context_traits_email = b.email
+   group by 1,2,3
 )
 
  select user_id
@@ -40,7 +40,9 @@ with user_ip_events as (
   group by 1,2,3
 ;
 
-create table etl.etl_core.user_event_ip_counts_test
+drop table if exists etl.etl_core.user_event_ip_counts_test_staging
+
+create table etl.etl_core.user_event_ip_counts_test_staging
 
 as
 
@@ -48,7 +50,7 @@ select user_id
      , ip
      , ipv4
      , num_events
-  from etl.etl_core.user_event_ip_counts
+  from etl.etl_core.user_event_ip_counts_staging
  where num_events_rank = 1
 ;
 
@@ -67,8 +69,12 @@ select current_timestamp, 'Swapping tables...';
 use role etl;
 create table if not exists etl.etl_core.user_event_ip_counts like etl.etl_core.user_event_ip_counts_staging;
 alter table etl.etl_core.user_event_ip_counts_staging swap with etl.etl_core.user_event_ip_counts;
-drop table if exists etl.etl_core.user_purchase_old;
+drop table if exists etl.etl_core.user_event_ip_counts_old;
 alter table etl.etl_core.user_event_ip_counts_staging rename to etl.etl_core.user_event_ip_counts_old;
+create table if not exists etl.etl_core.user_event_ip_counts_test like etl.etl_core.user_event_ip_counts_test_staging;
+alter table etl.etl_core.user_event_ip_counts_test_staging swap with etl.etl_core.user_event_ip_counts_test;
+drop table if exists etl.etl_core.user_event_ip_counts_test_old;
+alter table etl.etl_core.user_event_ip_counts_test_staging rename to etl.etl_core.user_event_ip_counts_test_old;
 
 -- Script End
 --
