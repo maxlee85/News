c262d0240de16346fa1acfdb70633a38430c6b42
maxlee85 <maxwellclee@gmail.com>
2022-02-03 20:20:23 -0500

    Update member_emails.sql

--- member_emails.sql
+++ member_emails.sql
@@ -4,28 +4,28 @@
 
 with user_list as (
         select distinct a.user_id
-          from (select user_id from aaptiv_core.aaptiv.subscription_facts_merged where merged_sub_exp_time > current_timestamp) a
-    inner join etl.etl_core.user_purchase_ip b
+          from (select user_id from db.schema.subscription_facts_merged where merged_sub_exp_time > current_timestamp) a
+    inner join db.schema.user_purchase_ip b
             on a.user_id = b.user_id
-    inner join (select * from aaptiv_core.geo_ip.ip_location where postal_code in (
-                  select postal_code from aaptiv_core.geo_ip.ny_metro_zipcodes)
+    inner join (select * from db.geo_ip.ip_location where postal_code in (
+                  select postal_code from db.geo_ip.ny_metro_zipcodes)
                ) c
             on b.ipv4 between c.network_start_integer and c.network_last_integer
 
     union
 
        select distinct a.user_id
-         from (select user_id from aaptiv_core.aaptiv.subscription_facts_merged where merged_sub_exp_time > current_timestamp) a
-   inner join etl.etl_core.user_event_ip_counts_test b
+         from (select user_id from db.schema.subscription_facts_merged where merged_sub_exp_time > current_timestamp) a
+   inner join db.schema.user_event_ip_counts_test b
            on a.user_id = b.user_id
-   inner join (select * from aaptiv_core.geo_ip.ip_location where postal_code in (
-                  select postal_code from aaptiv_core.geo_ip.ny_metro_zipcodes)
+   inner join (select * fromdb.geo_ip.ip_location where postal_code in (
+                  select postal_code from db.geo_ip.ny_metro_zipcodes)
               ) c
            on b.ipv4 between c.network_start_integer and c.network_last_integer
 )
 
   select email
-         from aaptiv_core.aaptiv.users_raw ur
+         from db.schema.users_raw ur
    inner join user_list ul
            on ur.user_id = ul.user_id
         where ur.email is not null
