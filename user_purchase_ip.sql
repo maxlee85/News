0a91cb9ce9fab0c95b5dd84588ff16957988ef62
maxlee85 <maxwellclee@gmail.com>
2022-02-03 20:24:54 -0500

    Update user_purchase_ip.sql

--- user_purchase_ip.sql
+++ user_purchase_ip.sql
@@ -4,9 +4,9 @@ use role etl;
 
 select current_timestamp, 'Creating Staging Table...';
 
-drop table if exists etl.etl_core.user_purchase_ip_staging;
+drop table if existsdb.schema.user_purchase_ip_staging;
 
-create table etl.etl_core.user_purchase_ip_staging
+create table db.schema.user_purchase_ip_staging
 
 as
 
@@ -16,7 +16,7 @@ with purchase_sources as (
           , a.context_ip as ip
           , parse_ip(a.context_ip, 'INET')['ipv4']::int as ipv4
        from segment_events.web_flows.action a
-  left join (select distinct email, metadata_id as user_id from aaptiv_core.stripe.customers) b
+  left join (select distinct email, metadata_id as user_id from db.stripe.customers) b
          on a.email = b.email
       where a.type = 'payment.PURCHASE_PLAN_SUCCESS'
         and a.path <> '/render2'
@@ -26,7 +26,7 @@ with purchase_sources as (
           , a.context_ip as ip
           , parse_ip(a.context_ip, 'INET')['ipv4']::int as ipv4
        from segment_events.web_flows.wsf_step a
-  left join (select distinct email, metadata_id as user_id from aaptiv_core.stripe.customers) b
+  left join (select distinct email, metadata_id as user_id from db.stripe.customers) b
          on strip_quotes(parse_url(a.context_page_url, 1):parameters:userEmail) = b.email
       where a.step = 'confirmation'
         and a.user_id is null
@@ -35,14 +35,14 @@ with purchase_sources as (
           , timestamp
           , context_ip as ip
           , parse_ip(context_ip, 'INET')['ipv4']::int as ipv4
-       from aaptiv_core.segment_ios.subscription_subscribe
+       from db.segment_ios.subscription_subscribe
       where status = 'success'
   union all
      select user_id
           , timestamp
           , context_ip as ip
           , parse_ip(context_ip, 'INET')['ipv4']::int as ipv4
-       from aaptiv_core.segment_android.subscription_subscribe
+       from db.segment_android.subscription_subscribe
       where status = 'success'
 )
 
@@ -52,7 +52,7 @@ with purchase_sources as (
          , p.ip
          , p.ipv4
       from purchase_sources p
- left join aaptiv_core.aaptiv.users_raw u on p.user_id = u.user_id
+ left join db.schema.users_raw u on p.user_id = u.user_id
      where p.user_id is not null
 ;
 
@@ -61,18 +61,18 @@ with purchase_sources as (
 select current_timestamp, 'Issuing Grants...';
 
 use role sysadmin;
-grant select on all tables in schema etl.etl_core to role etl;
-grant select on all tables in schema etl.etl_core to role aaptiv_core_ro;
+grant select on all tables in schema db.schema to role etl;
+grant select on all tables in schema db.schema to role role_name;
 
 -- Swap tables
 --
 select current_timestamp, 'Swapping tables...';
 
 use role etl;
-create table if not exists etl.etl_core.user_purchase_ip like etl.etl_core.user_purchase_ip_staging;
-alter table etl.etl_core.user_purchase_ip_staging swap with etl.etl_core.user_purchase_ip;
-drop table if exists etl.etl_core.user_purchase_old;
-alter table etl.etl_core.user_purchase_ip_staging rename to etl.etl_core.user_purchase_old;
+create table if not exists db.schema.user_purchase_ip like db.schema.user_purchase_ip_staging;
+alter table db.schema.user_purchase_ip_staging swap with db.schema.user_purchase_ip;
+drop table if exists db.schema.user_purchase_old;
+alter table db.schema.user_purchase_ip_staging rename to db.schema.user_purchase_old;
 
 -- Script End
 --
